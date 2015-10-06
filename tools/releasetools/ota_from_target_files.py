151439f326b90046ced920b6a115a5a3739664dc
Chris Soyars <ctso@ctso.me>
2015-10-06 18:25:03 -0400

    Add otapackage support for backuptool (squashed) - fix
    Squashed with:
    *Modular backuptool.sh. Executes backup and restore methods defined in /system/addon.d/*.sh scripts.
    *ota_from_target_files to handle mounting/unmounting for backup script
    *build: edify: use set_metadata for backuptool
    *ota: Fix recovery-script cleanups in block mode
    *build: ota: Support for install tools in /tmp/install
    *releasetools: clean up residual targetfiles-* dirs in /tmp/
    *releasetools: fix cleaning up /tmp/
    
    Conflicts:
    	core/Makefile
    	tools/releasetools/edify_generator.py
    	tools/releasetools/ota_from_target_files.py
    
    Change-Id: Id72896ff8c4b5d1cacc00237f976b55af00344c3
    
    Conflicts:
    	tools/releasetools/ota_from_target_files.py

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -178,6 +178,7 @@ OPTIONS.full_radio = False
 OPTIONS.full_bootloader = False
 # Stash size cannot exceed cache_size * threshold.
 OPTIONS.cache_size = None
+OPTIONS.backuptool = False
 OPTIONS.stash_threshold = 0.8
 OPTIONS.gen_verify = False
 OPTIONS.log_diff = None
@@ -547,6 +548,16 @@ def GetImage(which, tmpdir, info_dict):
   return sparse_img.SparseImage(path, mappath, clobbered_blocks)
 
 
+def CopyInstallTools(output_zip):
+  oldcwd = os.getcwd()
+  os.chdir(os.getenv('OUT'))
+  for root, subdirs, files in os.walk("install"):
+    for f in files:
+      p = os.path.join(root, f)
+      output_zip.write(p, p)
+  os.chdir(oldcwd)
+
+
 def WriteFullOTAPackage(input_zip, output_zip):
   # TODO: how to determine this?  We don't know what version it will
   # be installed on top of. For now, we expect the API just won't
@@ -639,9 +650,6 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("Target: %s" % CalculateFingerprint(
       oem_props, oem_dict, OPTIONS.info_dict))
 
-  script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
-  device_specific.FullOTA_InstallBegin()
-
   script.Print("")
   script.Print("       || THANK YOU FOR FLASHING ||        ")
   script.Print("")
@@ -664,6 +672,14 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("")
   script.Print("          ||| ANDROID 6.0.1 |||           ")
   script.Print("")
+
+  script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
+  device_specific.FullOTA_InstallBegin()
+
+  CopyInstallTools(output_zip)
+  script.UnpackPackageDir("install", "/tmp/install")
+  script.SetPermissionsRecursive("/tmp/install", 0, 0, 0755, 0644, None, None)
+  script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0755, 0755, None, None)
 
   if OPTIONS.backuptool:
     script.Mount("/system")
@@ -757,6 +773,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
                    ""+input_zip.read("SYSTEM/addon.d/UPDATE-SuperSU.zip"))
     script.Mount("/system")
     script.FlashSuperSU()
+
   if block_based:
     script.Unmount("/system")
 
